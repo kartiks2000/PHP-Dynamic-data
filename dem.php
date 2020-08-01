b981c1d51506380ea6007df4bab81c1198b65f75
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-01 21:54:30 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $title = "This is kartik" ?>
+    <h1>here's the title...</h1>
+    <?php echo $title; ?>
+</body>
+</html>
